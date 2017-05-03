9b3dab710f8746e13eda533f89bd4e914d6d2b98
Student 5 <student5@Felix-P-05.local>
2017-05-03 12:29:51 +0530

    Initial Commit

---
+++ AudioVideoIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioVideoIOS
+//
+//  Created by Student 5 on 03/05/17.
+//  Copyright © 2017 Student 5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioVideoIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioVideoIOS
+//
+//  Created by Student 5 on 03/05/17.
+//  Copyright © 2017 Student 5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
